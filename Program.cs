a25bb8e5a6c6afa70e1f2bafa4e04a7e749d0e5c
izoo0 <isaiahwainaina34@mgmail.com>
2023-02-15 04:08:27 +0300

    complete

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace calculator{
+     class Calculator{
+          public static int a;
+          public static int b;
+
+         
+          public static void Main(string[] args)
+          {
+               Console.WriteLine("Select the operation");
+               Console.WriteLine(
+                    "1. Multiplication\n"+
+                    "2. Addition\n" +
+                    "3. Subtraction\n"+
+                    "4. Division"
+               );
+
+               int option = Convert.ToInt32(Console.ReadLine());
+
+               if(option == 1){
+                    Console.WriteLine("Enter the first number");
+                    a = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Enter the Second number");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    int c = a*b;
+                    Console.WriteLine("The answer after" +" " +option +" " +c);
+               }else
+               if(option == 2){
+                    Console.WriteLine("Enter the first number");
+                    a = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Enter the Second number");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    int c = a+b;
+                    Console.WriteLine("The answer after" +" " +option +" " +c);
+               }else
+               if(option == 3){
+                    Console.WriteLine("Enter the first number");
+                    a = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Enter the Second number");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    int c = a-b;
+                    Console.WriteLine("The answer after" +" " +option +" " +c);
+               }else
+               if(option == 4){
+                    Console.WriteLine("Enter the first number");
+                    a = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Enter the Second number");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    int c = a/b;
+                    Console.WriteLine("The answer after" +" " +option +" " +c);
+               }else
+               {
+                    Console.WriteLine("Please Select one option");
+               }
+          }
+     }
+}
